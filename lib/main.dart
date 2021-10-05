9106cb5e5fb04b765823ef3ab58699097358cd3f
Gabrielly Rodrigues <gabyrlsilva@lapisco.ifce.edu.br>
2021-10-05 18:26:17 -0300

    First Project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+
+import 'package:Conversor_de_Moedas/app/views/home_view.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: HomeView(),
+    );
+  }
+}
